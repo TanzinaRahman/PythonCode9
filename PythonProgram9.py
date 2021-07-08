5eb319002c6d2a49140ffb873fb97e23a7134eaa
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-08 21:51:12 +0600

    Create PythonProgram9.py

---
+++ PythonProgram9.py
@@ -0,0 +1,6 @@
+# Rectangle ; Area = w * h ; [ w = width , h = height ]
+
+width = float(input("Enter width : "))
+height = float(input("Enter height : "))
+Area = width * height
+print("\nArea is = ", Area)
